4d84aed6b3f007e2a57aebe299d6300628ead57f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-02 23:35:27 +0500

    Chakra UI

---
+++ src/pages/index.jsx
@@ -0,0 +1,37 @@
+import {
+  Flex,
+  Spacer,
+  Center,
+  Text,
+  Square,
+  Box,
+  VStack,
+  Button,
+  Grid,
+  HStack,
+  Stack,
+  Heading
+} from '@chakra-ui/react'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div>
+
+   
+<Button
+  colorScheme="red"
+  _hover={{
+    background: "red.300",
+    color: "white.500",
+  }}
+>
+  Hover me
+</Button>
+
+   
+    </div>
+  )
+}
+
+export default Index
