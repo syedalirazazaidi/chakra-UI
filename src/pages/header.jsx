b0ab60a579c4f8821c1a460d40b50fd533bf9d1c
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-10-07 16:28:29 +0500

    add reponsiveness on UI

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -3,21 +3,22 @@ import React from 'react'
 const Header = () => {
   return (
     <Box as="section" bg="purple.600" w="100%" h="397px" color="gray.50">
-      <VStack textAlign={['left','left','center']}>
+      <VStack textAlign={['left', 'left', 'center']}>
         <Heading
-          w={['366px', "751px", "751px"]}
+          w={['366px', '751px', '751px']}
           fontSize={['3xl', '3xl', '5xl']}
           mt="40.45px"
-         ml={[ "90px","355px","355px"]}
-          
+          ml={['90px', '85px', '85px']}
           fontWeight="extrabold"
         >
           I'm overriding this heading
         </Heading>
-        <Text 
-        w={['280px','0px','100%']}
-        
-        fontSize={[ '15px','20px','20px']}  fontWeight="medium" pt="4">
+        <Text
+          w={['280px', '0px', '100%']}
+          fontSize={['15px', '20px', '20px']}
+          fontWeight="medium"
+          pt="4"
+        >
           Plans that are carefully crafted to suit your business.
         </Text>
       </VStack>
